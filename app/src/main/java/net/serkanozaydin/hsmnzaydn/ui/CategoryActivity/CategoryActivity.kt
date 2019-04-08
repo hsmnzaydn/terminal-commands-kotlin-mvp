ae28fc324381ef901916baf2cabd70423f642a0e
Your Name <you@example.com>
2019-04-08 17:16:34 +0300

    Added navigation to launcher activity

--- app/src/main/java/net/serkanozaydin/hsmnzaydn/ui/CategoryActivity/CategoryActivity.kt
+++ app/src/main/java/net/serkanozaydin/hsmnzaydn/ui/CategoryActivity/CategoryActivity.kt
@@ -2,9 +2,17 @@ package net.serkanozaydin.hsmnzaydn.ui.CategoryActivity
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.LinearLayout
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_navigation.*
+import kotlinx.android.synthetic.main.app_bar_navigation.*
+import kotlinx.android.synthetic.main.content_navigation.*
 import net.serkanozaydin.hsmnzaydn.MvpApp
 import net.serkanozaydin.hsmnzaydn.R
 import net.serkanozaydin.hsmnzaydn.Utility.BUNDLE_CATEGORY_ID
@@ -14,19 +22,34 @@ import net.serkanozaydin.hsmnzaydn.ui.adapters.CategoryRecylerviewAdapter
 import net.serkanozaydin.hsmnzaydn.ui.base.BaseActivity
 import javax.inject.Inject
 
-class CategoryActivity : BaseActivity(),CategoryActivityMvpView {
+class CategoryActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener,CategoryActivityMvpView  {
 
-    lateinit var adapter:CategoryRecylerviewAdapter
-    lateinit var changeClass:Intent
+    lateinit var adapter: CategoryRecylerviewAdapter
+    lateinit var changeClass: Intent
 
 
     @Inject
     lateinit var presenter: CategoryActivityMvpPresenter<CategoryActivityMvpView>
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_navigation)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
 
         (application as MvpApp).getActivityComponent()!!.injectMainActivity(this)
 
@@ -36,7 +59,7 @@ class CategoryActivity : BaseActivity(),CategoryActivityMvpView {
 
         adapter= CategoryRecylerviewAdapter(object: CategoryRecylerviewAdapter.ItemListener{
             override fun onItemClick(item: Category) {
-                changeClass= Intent(this@CategoryActivity,CommandListActivity::class.java)
+                changeClass= Intent(this@CategoryActivity, CommandListActivity::class.java)
                 changeClass.putExtra(BUNDLE_CATEGORY_ID,item.id)
                 startActivity(changeClass)
             }
@@ -44,11 +67,59 @@ class CategoryActivity : BaseActivity(),CategoryActivityMvpView {
         })
     }
 
-
     override fun loadDataToList(response: List<Category>?) {
         adapter.setData(response)
-        activity_main_recyler_view.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL,false)
-        activity_main_recyler_view.adapter=adapter
+        content_navigation_recylerview.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL,false)
+        content_navigation_recylerview.adapter=adapter
+    }
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.navigation, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
