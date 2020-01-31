606e306326c8cd2357541868f0f4b7769086b455
Serkan <serkan@basefy.com>
2020-01-31 18:47:24 +0300

    Change base path

--- app/src/main/java/net/serkanozaydin/hsmnzaydn/Utility/Configuration.kt
+++ app/src/main/java/net/serkanozaydin/hsmnzaydn/Utility/Configuration.kt
@@ -2,7 +2,8 @@ package net.serkanozaydin.hsmnzaydn.Utility
 
 
 //val BASE_SERVER_URL:String="http://192.168.4.207:8080/api/";
-val BASE_SERVER_URL:String="http://165.22.48.124:8080/api/";
+//val BASE_SERVER_URL:String="http://165.22.48.124:8080/api/";
+val BASE_SERVER_URL:String="https://termcommandpro.herokuapp.com/api/";
 
 // Network Configuration
 val readTimeOut:Long = 0
